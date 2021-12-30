2f026391ce7da33b67017f4002c35275be5aa8c1
Mayank Kumar <mayank.zxc@gmail.com>
2021-12-30 20:29:11 +0530

    added frontend components

---
+++ src/Components/Announcements.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+`;
+
+const Announcements = () => {
+    return <Container>Get 50% off</Container>;
+};
+
+export default Announcements;

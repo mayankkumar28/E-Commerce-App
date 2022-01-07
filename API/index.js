cfbe5f49cf2d377ca1d86cb1c081b92d9d93e823
Mayank Kumar <mayank.zxc@gmail.com>
2022-01-07 23:07:35 +0530

    added api

---
+++ API/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("db connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("backend server is running");
+});

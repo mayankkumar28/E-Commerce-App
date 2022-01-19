139e6858d4f8d2a9385ca35c0cfb9dc467337686
Mayank Kumar <mayank.zxc@gmail.com>
2022-01-19 23:21:37 +0530

    final commit

---
+++ API/client/src/components/Footer.jsx
@@ -0,0 +1,205 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1`
+  color: black;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+  color: inherit !important;
+  text-decoration: inherit;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>MY CLOTH SHOP</Logo>
+        <Desc>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don’t look even slightly believable.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              Home
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/cart" style={{ textDecoration: "none", color: "black" }}>
+              Cart
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              to="/products/men"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Men fashion
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              to="/products/women"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Women Fashion
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              to="/products/ "
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Accessories
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/cart" style={{ textDecoration: "none", color: "black" }}>
+              My Account
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/cart" style={{ textDecoration: "none", color: "black" }}>
+              Order Tracking
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              to="/login"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Login
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              to="/register"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Register
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              Terms
+            </Link>
+          </ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> 1232, San Jose , California
+          98336
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +1 234 56 789
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} />{" "}
+          contact@myclothshop.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ API/client/src/data.js
@@ -0,0 +1,44 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.imgur.com/OpuYLwN.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/pKCI8WC.jpg?1",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.imgur.com/TFWtM7Y.jpg",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.imgur.com/AzGZBNG.jpg",
+    title: "LONG COATS",
+    cat: "coat",
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/9zXmF6d.jpg",
+    title: "SHIRT STYLE!",
+    cat: "tshirt",
+  },
+  {
+    id: 3,
+    img: "https://i.imgur.com/zuMG1eE.jpg",
+    title: "LIGHT JACKETS",
+    cat: "jacket",
+  },
+];
